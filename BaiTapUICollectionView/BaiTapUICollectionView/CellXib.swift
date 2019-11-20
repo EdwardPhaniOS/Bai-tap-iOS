befa29bbe1e6e32e3c05b5ec1170f601c99e48ae
iMac_VTCA <imac_vtca@iMac-VTCAs-iMac-8.local>
2019-11-20 19:38:39 +0700

    Save BTCollectionView

---
+++ BaiTapUICollectionView/BaiTapUICollectionView/CellXib.swift
@@ -0,0 +1,18 @@
+//
+//  CellXib.swift
+//  BaiTapUICollectionView
+//
+//  Created by iMac_VTCA on 11/20/19.
+//  Copyright © 2019 PTV. All rights reserved.
+//
+
+import UIKit
+
+class CellXib: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- BaiTapUICollectionView/BaiTapUICollectionView/MyCollectionViewController.swift
+++ BaiTapUICollectionView/BaiTapUICollectionView/MyCollectionViewController.swift
@@ -15,13 +15,8 @@ class MyCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        if let flowLayout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
-//            flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
-//        }
-        
-        if let flowLayou = collectionViewLayout as? UICollectionViewFlowLayout {
-            flowLayou.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
-        }
+        let nib = UINib(nibName: "CellXib", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "CellXib")
     }
     
     // MARK: UICollectionViewDataSource
@@ -38,16 +33,8 @@ class MyCollectionViewController: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! MyCollectionViewCell
-        cell.rowNumber = indexPath.row
         
-        if indexPath.row % 2 == 0 {
-            cell.contentView.frame = CGRect(x: 0, y: 0, width: 60, height: 60)
-            cell.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
-        } else {
-            cell.contentView.frame = CGRect(x: 0, y: 0, width: 60, height: 120)
-            cell.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-        }
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellXib", for: indexPath)
         
         return cell
     }
@@ -59,14 +46,7 @@ extension MyCollectionViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 
-        let width = 60
-        let heigh = 60
-
-        if indexPath.row % 2 == 0 {
-            return CGSize(width: width, height: heigh)
-        } else {
-            return CGSize(width: width, height: heigh*2)
-        }
+        return CGSize(width: 50, height: 70)
     }
     
     
