33188b2df44a32cd68adad60a8f7f72c4a8ad7f2
iMac_VTCA <imac_vtca@iMac-VTCAs-iMac-80.local>
2019-11-25 21:13:40 +0700

    Them bai Autolayout

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by iMac_VTCA on 11/25/19.
+//  Copyright © 2019 iMac_VTCA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+
+}
+
