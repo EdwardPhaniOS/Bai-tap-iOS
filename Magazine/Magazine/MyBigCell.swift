bfe2285d70bb7c7edfe5c30cfe048653b2955bbd
EdwardPhaniOS <EdwardPhan2015@gmail.com>
2019-11-27 14:37:44 +0700

    Them BT Magazine

---
+++ Magazine/Magazine/MyBigCell.swift
@@ -0,0 +1,16 @@
+//
+//  MyBigCell.swift
+//  Magazine
+//
+//  Created by Tan Vinh Phan on 11/27/19.
+//  Copyright © 2019 PTV. All rights reserved.
+//
+
+import UIKit
+
+class MyBigCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    
+}
---
+++ Magazine/Magazine/MySmallCell.swift
@@ -0,0 +1,21 @@
+//
+//  MySmallCell.swift
+//  Magazine
+//
+//  Created by Tan Vinh Phan on 11/27/19.
+//  Copyright © 2019 PTV. All rights reserved.
+//
+
+import UIKit
+
+class MySmallCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+//    func setupView(imageName: String = "flocks_of_bird") {
+//        
+//        let image = UIImage(named: imageName)
+//        imageView.image = image
+//        imageView.contentMode = .scaleAspectFill
+//    }
+}
